8b910ac033f95eb4fea22eb53745d92f7345f376
ItsMashBro <113943485+ItsMashBro@users.noreply.github.com>
2022-10-03 02:11:02 -0500

    Final

--- Script.js
+++ Script.js
@@ -5,8 +5,11 @@ let shinigami = {
     JapanPopulation: 1000 //Obviously not but I'm not making the game run up to 125 million lol
 }
 let misaButton = document.getElementById("buymisa")
+    misaButton.onclick = buyMisa;
 let kyosukeButton = document.getElementById("buykyosuke")
+    kyosukeButton.onclick = buyKyosuke;
 let teruButton = document.getElementById("buyteru")
+    teruButton.onclick = buyTeru;
 
 //Define Functions
     //Write button and fame counter
@@ -21,26 +24,27 @@ function gainFame(){
 }
 //Buy buttons and incrementers
 
-setInterval(function buyMisa() {
-    if (shinigami.fame >= 10){
-        shinigami.names = shinigami.names + 1;
-        document.getElementById("namecount").innerHTML = shinigami.names;
-}}, 1000)
+function buyMisa(){
+    setInterval(function() {
+        if (shinigami.fame >= 10){
+            shinigami.names = shinigami.names + 5;
+            document.getElementById("namecount").innerHTML = shinigami.names;
+}}, 1000)}
 
 
+function buyKyosuke(){
+    setInterval(function() {
+        if (shinigami.fame >= 50){
+            shinigami.names = shinigami.names + 10;
+            document.getElementById("namecount").innerHTML = shinigami.names;
+    }}, 1000)}
 
-setInterval(function buyHiguchi() {
-    if (shinigami.fame >= 50){
-        shinigami.names = shinigami.names + 3;
-        document.getElementById("namecount").innerHTML = shinigami.names;
-}}, 1000)
-
-
-setInterval(function buyTeru() {
-    if (shinigami.fame >= 85){
-        shinigami.names = shinigami.names + 5;
-        document.getElementById("namecount").innerHTML = shinigami.names;
-}}, 1000)
+function buyTeru(){
+    setInterval(function() {
+        if (shinigami.fame >= 85){
+            shinigami.names = shinigami.names + 20;
+            document.getElementById("namecount").innerHTML = shinigami.names;
+    }}, 1000)}
 
 
 
